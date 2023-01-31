b709b2dec13bb66fb77dc9f76b837ee99bc49dfe
jose <jose.brito@maisunifacisa.com.br>
2023-01-31 00:31:47 -0300

    att carService seta valores iniciais

--- test.java/src/main/java/com/ws/work/java/test/java/service/CarService.java
+++ test.java/src/main/java/com/ws/work/java/test/java/service/CarService.java
@@ -4,8 +4,10 @@ import com.ws.work.java.test.java.domain.Car;
 import com.ws.work.java.test.java.domain.dto.ListAllCarsDTO;
 import com.ws.work.java.test.java.domain.mapper.ListAllCarsMapper;
 import com.ws.work.java.test.java.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,13 +16,21 @@ public class CarService {
 
     private final CarRepository carRepository;
 
+    @Autowired
+    private final ModelService modelService;
+
     private final ListAllCarsMapper carsMapper = new ListAllCarsMapper();
 
-    public CarService(CarRepository carRepository) {
+    public CarService(CarRepository carRepository, ModelService modelService) {
         this.carRepository = carRepository;
+        this.modelService = modelService;
     }
 
     public Car save(Car car) {
+        car.setTimestampRegistry(new Timestamp(System.currentTimeMillis()));
+        if(car.getModel() != null)
+            car.setModel(modelService.findById(car.getModel().getId()));
+
         return carRepository.save(car);
     }
 
